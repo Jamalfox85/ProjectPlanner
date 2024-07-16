da8956f2d4359fe7879b45b93df9ed09090ce219
Jamal <Fox.Jamal@outlook.com>
2024-07-16 19:31:55 -0400

    env updates

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient("https://mruowxbmzmzydlagozsj.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ydW93eGJtem16eWRsYWdvenNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAxMzczODMsImV4cCI6MjAzNTcxMzM4M30.qa-juTHhz8SN5o8ZYaWZO7xsPp7wxPij64Rtm4ui3Qo");
+export const supabase = createClient("https://mruowxbmzmzydlagozsj.supabase.co", import.meta.env.VITE_SUPABASE_CLIENT_KEY);
